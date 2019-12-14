572591cfab7a7e33de98acdcea5b2416bd3d4643
RLittlesII <6969701+RLittlesII@users.noreply.github.com>
2019-12-14 10:30:31 -0600

    Added unit tests

---
+++ test/ObservR.Tests/ObserverTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace ObservR.Tests
+{
+    public sealed class ObserverTests
+    {
+        public class TheSendMethod
+        {
+        }
+
+        public class ThePublishMethod
+        {
+        }
+    }
+}
